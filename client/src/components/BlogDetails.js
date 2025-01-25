b9cad78f848c86832dc36d15a363d946054994a6
furqanahmadejaz <furqanahmadejaz03@gmail.com>
2025-01-25 13:46:53 +0530

    new changes

--- client/src/components/BlogDetails.js
+++ client/src/components/BlogDetails.js
@@ -2,6 +2,16 @@ import { useState, useEffect } from "react"
 
 
 const BlogDetails = ({blog}) => {
+
+    const blogPreview = blog.body.substring(0, 400) + "...";
+    const [blogBody, setBlogBody] = useState(blogPreview)
+    const [show, setShow] = useState("show more")
+    const [expand, setExpand] = useState(false)
+
+
+    
+
+
     
     const handleClick = async() => {
         const response = await fetch('http://localhost:4000/api/blogs/' + blog._id, {
@@ -12,21 +22,34 @@ const BlogDetails = ({blog}) => {
             console.log(response)
         }
     }
+    const handleBlogClick = () =>{
+        
+        if(expand){
+            setBlogBody(blogPreview);
+            
+            
+            setShow("show more")
+            setExpand(false)
+        } else{
+            setBlogBody(blog.body);
+            setShow('show less')
+            setExpand(true)
+        }
+
+    }
 
 
-    return (  
-        <div className="blog-details">
-            <div className="blog-title">
-                {blog.title}
-            </div>
-            <div className="blog-body">
-                {blog.body}
-            </div>
-            <div className="blog-author">
-                {blog.author}
-            </div>
-            <span onClick = {handleClick}>Delete Blog</span>
+    return (
+      <div className="blog-details">
+        <div className="blog-title">{blog.title}</div>
+        <div className="blog-body">{blogBody}</div>
+        <div className="expand" onClick={handleBlogClick}>
+          {show}
         </div>
+
+        <div className="blog-author">{blog.author}</div>
+        <span onClick={handleClick}>Delete Blog</span>
+      </div>
     );
 }
  
